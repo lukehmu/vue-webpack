9912aa3d8d8c925d1f08c15bbea5ffd3a5711f7c
Luke <lukehmu@gmail.com>
2019-08-13 23:58:34 +0100

    footer layout

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-    <LayoutHeader pageName="hello"/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LayoutHeader pageName="hello" />
+    <HelloWorld msg="Body here" />
+    <LayoutFooter orgName="footer company name"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import LayoutHeader from './components/LayoutHeader.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import LayoutHeader from './components/LayoutHeader.vue'
+  import LayoutFooter from './components/LayoutFooter.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    LayoutHeader
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+      LayoutHeader,
+      LayoutFooter
+    }
   }
-}
+
 </script>
 
 <style>
